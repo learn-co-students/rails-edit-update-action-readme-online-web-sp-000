74c3588a1492960e97f0bf8aa5e999d1725e3850
maxwellbenton <maxwellbenton@gail.com>
2018-08-27 14:28:09 -0400

    added solution

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  # add edit and update methods here
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    raise params.inspect
+  end
+end
