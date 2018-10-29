cc78916882216ea17361d836cd87bbbd08a174d1
Graham <telegraham@users.noreply.github.com>
2018-10-29 14:18:24 -0400

    Revert "Posts to articles solution (#20)"
    This reverts commit a16f8661a3db55fd32a294d4b523eb46237bc317.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 
 ActiveRecord::Schema.define(version: 20151230232412) do
 
-  create_table "articles", force: :cascade do |t|
+  create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
