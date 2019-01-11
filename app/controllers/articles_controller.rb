eecda8a78020d9c82563a9298d77ed7f8cb9cfe9
Christopher Tompkins <chtompkins026@gmail.com>
2019-01-11 21:25:51 +0000

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,4 +20,15 @@ class ArticlesController < ApplicationController
   end
 
   # add edit and update methods here
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
+ def update
+   @article = Article.find(params[:id])
+    @article.update(params.require(:article))
+    redirect_to article_path(@article)
+  end
+  
 end
