94cf99fb8b1d4d931bf65da2a63a63db3ac36426
Cookie McCormick <muntananuchat@gmail.com>
2019-03-11 14:03:10 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :articles, only: %i[index show new create edit update]
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
 end
