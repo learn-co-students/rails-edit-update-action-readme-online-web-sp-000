a92740bb4aedef77fb2ba230cb88d010d9a4917e
Ramon Chavez <rachavez1974@gmail.com>
2019-08-11 23:31:16 -0400

    Add update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,10 +21,25 @@ class ArticlesController < ApplicationController
     redirect_to article_path(@article)
   end
 
+  def edit
+    
+  end
+
+  def update
+    @article.update(article_params)
+    redirect_to @article
+  end
+
 
   # add edit and update methods here
   private
     def set_coupon
       @article = Article.find(params[:id])
     end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
+
+
 end
