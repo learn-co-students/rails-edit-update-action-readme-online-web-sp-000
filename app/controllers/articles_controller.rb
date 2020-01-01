75935bb33393198590a110384aa5eb25d4824932
Ronnie Stephens <rstephens@bluemorpho.cc>
2020-01-01 12:20:01 -0500

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+
   def index
     @articles = Article.all
   end
@@ -9,6 +10,16 @@ class ArticlesController < ApplicationController
 
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
   end
 
   def create
