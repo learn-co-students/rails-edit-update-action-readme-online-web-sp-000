671b3edbbe1d069d9b0eb17674e1a9a5e29ebc34
azbury <acasbury25@aol.com>
2020-01-13 13:40:37 -0500

    adding get route for edit article

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
 end
