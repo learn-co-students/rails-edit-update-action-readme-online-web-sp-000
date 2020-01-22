750153bb36c2bafedae3b5cc5365a4df2f8946b2
Chinedu Arinze <c.s.arinze@gmail.com>
2020-01-22 11:14:54 -0500

    Add resources to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  resources :articles, only: [:index, :show, :update, :create]
   get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
   patch 'articles/:id', to: 'articles#update'
 end
