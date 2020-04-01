a667790805db2d56bf4dcce9c2307e6420bb38b2
Ralph85 <Ralph.saintalbord@yahoo.com>
2020-04-01 15:01:30 -0400

    Done.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.description = params[:description]
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  # add edit and update methods here
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(title: params[:post][:title], description: params[:post][:description])
+    redirect_to post_path(@post)
+  end
+end
