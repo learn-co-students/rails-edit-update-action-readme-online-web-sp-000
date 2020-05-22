ec451cca90a2a74a7ec9c9241922dd524fb8c637
Jason J Cruz <jaycru06@yahoo.com>
2020-05-22 03:57:46 +0000

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
@@ -12,25 +13,28 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new
-    @article.title = params[:title]
-    @article.description = params[:description]
+    @article = Article.new(article_params)
+    # @article.title = article_params[:title]
+    # @article.description = article_params[:description]
     @article.save
     redirect_to article_path(@article)
   end
 
   # add edit and update methods here
   def edit 
-    @article = Article.find(params[:id])
+    @article = Article.find(article_params[:id])
   end 
 
   def update 
-    @article = Article.find(params[:id])
-    @article.update(title: params[:article][:title], 
-                    description: params[:article][:description]
-                    )
+    @article = Article.find(article_params[:id])
+    @article.update(article_params)
     redirect_to article_path(@article)
   end 
+  
+  private 
+    def article_params 
+      params.permit(:id, :title, :description)
+    end 
 end
 
 
