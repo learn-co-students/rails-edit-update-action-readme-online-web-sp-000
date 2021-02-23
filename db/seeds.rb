eb449f2f361358b069e487376d42ff36253045f2
RSigmaSG <sg.comparch@gmail.com>
2021-02-23 17:42:02 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create(title: 'title_1', description: 'description_1')
