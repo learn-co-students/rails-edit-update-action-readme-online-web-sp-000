e2157d6b2dce2c7a9c40ca40256f9ef2eed5fb13
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2021-05-09 12:05:50 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.create([{title: 'Article 1', description: 'Article 1 Description'},
+{title: 'Article 2', description: 'Article 2 Description'},
+{title: 'Article 3', description: 'Article 3 Description'},
+{title: 'Article 4', description: 'Article 4 Description'},
+{title: 'Article 5', description: 'Article 5 Description'},
+{title: 'Article 6', description: 'Article 6 Description'},
+{title: 'Article 7', description: 'Article 7 Description'},
+{title: 'Article 8', description: 'Article 8 Description'},
+{title: 'Article 9', description: 'Article 9 Description'},
+{title: 'Article 10', description: 'Article 10 Description'}])
